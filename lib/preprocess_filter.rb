12bc0d616af8f1c9884c252365f69813d745a64f
Simone Carletti <weppos@weppos.net>
2014-04-03 00:00:25 +0200

    Add markers support

---
+++ lib/preprocess_filter.rb
@@ -0,0 +1,18 @@
+class PreprocessFilter < Nanoc3::Filter
+  identifier :preprocess
+
+  REGEXP_MARKERS = /<(warning|note|info)>(.*?)<\/\1>/m
+
+  def run(content, params = {})
+    content = content.dup
+    format_markers content
+    content
+  end
+
+private
+
+  def format_markers(content)
+    content.gsub!(REGEXP_MARKERS) { %(<div class="marker marker-#{$1}" markdown="1">#{$2}</div>) }
+  end
+
+end
