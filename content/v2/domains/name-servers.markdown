dnsimple/dnsimple-developer

commit 513edd2f81caaa3751049959a5415760b3593f34

Make v2 docs consistent

content/v2/domains/name-servers.markdown
@@ -1,14 +1,12 @@
----
-title: Domain | Name Servers | DNSimple API v2
 excerpt: This page documents the DNSimple domain name servers API v2.
 ---
 
-# Domain Name Servers API
-
-Retrieve and manage delegation for a domain in DNSimple.
+# Domain name servers API
 
 * TOC
 {:toc}
+
+Retrieve and manage delegation for a domain in DNSimple.
 
 
 ## List name servers for a domain {#list}

content/v2/contacts.markdown
@@ -3,7 +3,7 @@ title: Contacts | DNSimple API v2
 excerpt: This page documents the DNSimple contacts API v2.
 ---
 
-# Contact API
+# Contacts API
 
 * TOC
 {:toc}

content/v2/domains.markdown
@@ -3,7 +3,7 @@ title: Domains | DNSimple API v2
 excerpt: This page documents the DNSimple domains API v2.
 ---
 
-# Domain API
+# Domains API
 
 * TOC
 {:toc}

content/v2/domains/email-forwards.markdown
@@ -1,9 +1,9 @@
 ---
-title: Email Forwards | DNSimple API v2
+title: Email forwards | Domains | DNSimple API v2
 excerpt: This page documents the DNSimple email forwards API v2.
 ---
 
-# Email Forwards API
+# Email forwards API
 
 * TOC
 {:toc}

content/v2/registrar/auto-renewal.markdown
@@ -1,9 +1,9 @@
 ---
-title: Domain registration auto-renewal
+title: Domain auto-renewal | Registrar | DNSimple API v2
 excerpt: This page documents the DNSimple domain registration auto-renewal API v2.
 ---
 
-# Domain registration auto-renewal API
+# Domain auto-renewal API
 
 * TOC
 {:toc}

content/v2/registrar/whois-privacy.markdown
@@ -1,21 +1,21 @@
 ---
-title: Whois Privacy | DNSimple API v2
-excerpt: This page documents the DNSimple whois privacy API v2.
+title: Whois privacy | DNSimple API v2
+excerpt: This page documents the DNSimple WHOIS privacy API v2.
 ---
 
-# Whois Privacy API
-
-Enable and disable whois privacy on registered domains.
+# Whois privacy API
 
 * TOC
 {:toc}
 
+Enable and disable WHOIS privacy on registered domains.
 
-## Get whois privacy for a domain {#get}
+
+## Get WHOIS privacy for a domain {#get}
 
     GET /:account/domains/:domain/whois_privacy
 
-Get the whois privacy details for a domain.
+Get the WHOIS privacy details for a domain.
 
 ### Parameters
 
@@ -26,7 +26,7 @@ Name | Type | Description
 
 ### Example
 
-Whois privacy for the domain example.com in the account 1010.
+Get the WHOIS privacy for the domain example.com in the account 1010.
 
     curl  -H 'Authorization: Bearer <token>' \
           -H 'Accept: application/json' \
@@ -34,17 +34,23 @@ Whois privacy for the domain example.com in the account 1010.
 
 ### Response
 
-Responds with HTTP 200 if whois privacy is purchased for the domain.
+Responds with HTTP 200 if WHOIS privacy is purchased for the domain.
 
 ~~~js
 {}
 ~~~
 
-Responds with HTTP 404 if whois privacy is not purchased.
+Responds with HTTP 404 if WHOIS privacy is not purchased.
 
 ## Enable whois privacy {#enable}
 
       PUT /:account/domains/:domain/whois_privacy
+
+Note that if the WHOIS privacy is not purchased for the domain, enabling WHOIS
+privacy will cause the service to be purchased for a period of 1 year.
+
+If WHOIS privacy was previously purchased and disabled, then calling this will
+enable the WHOIS privacy.
 
 ### Parameters
 
@@ -55,15 +61,7 @@ Name | Type | Description
 
 ### Example
 
-Enable whois privacy for the domain example.com.
-
-Note that if the whois privacy is not purchased for the domain, enabling whois
-privacy will cause the service to be purchased for a period of 1 year.
-
-If whois privacy was previously purchased and disabled, then calling this will
-enable the whois privacy.
-
-Whois privacy for the domain example.com in the account 1010.
+Enable WHOIS privacy for the domain example.com in the account 1010.
 
     curl  -H 'Authorization: Bearer <token>' \
           -H 'Accept: application/json' \
@@ -72,10 +70,9 @@ Whois privacy for the domain example.com in the account 1010.
 
 ### Response
 
-Responds with HTTP 201 if whois privacy is purchased and enabled.
+Responds with HTTP 201 if WHOIS privacy is purchased and enabled.
 
-Responds with HTTP 200 if whois privacy is only enabled because it was purchased
-previously.
+Responds with HTTP 200 if WHOIS privacy is only enabled because it was purchased previously.
 
 ~~~js
 {}
@@ -85,6 +82,12 @@ previously.
 
       DELETE /:account/domains/:domain/whois_privacy
 
+Note that if the WHOIS privacy is not purchased for the domain, this method will
+do nothing.
+
+If WHOIS privacy was previously purchased and enabled, then calling this will
+disable the WHOIS privacy.
+
 ### Parameters
 
 Name | Type | Description
@@ -94,15 +97,7 @@ Name | Type | Description
 
 ### Example
 
-Disable whois privacy for the domain example.com.
-
-Note that if the whois privacy is not purchased for the domain, this method will
-do nothing.
-
-If whois privacy was previously purchased and enabled, then calling this will
-disable the whois privacy.
-
-Whois privacy for the domain example.com in the account 1010.
+Disable whois privacy for the domain example.com in the account 1010.
 
     curl  -H 'Authorization: Bearer <token>' \
           -H 'Accept: application/json' \
@@ -111,7 +106,7 @@ Whois privacy for the domain example.com in the account 1010.
 
 ### Response
 
-Responds with HTTP 200 if whois privacy is disabled.
+Responds with HTTP 200 if WHOIS privacy is disabled.
 
 ~~~js
 {}

content/v2/services.markdown
@@ -8,6 +8,7 @@ excerpt: This page documents the DNSimple services API v2.
 * TOC
 {:toc}
 
+
 ## List services {#list}
 
     GET /services

content/v2/services/domains.markdown
@@ -1,9 +1,9 @@
 ---
-title: Domain Services | DNSimple API v2
+title: Domain services | Services | DNSimple API v2
 excerpt: This page documents the DNSimple domain services API v2.
 ---
 
-# Domain Services API
+# Domain services API
 
 * TOC
 {:toc}

commit 64a90e3c8f5b1d56c421cb1081bd76a0ae74003e

Fix formatting issue introduced in 513edd2f81caaa3751049959a5415760b3593f34

content/v2/domains/name-servers.markdown
@@ -1,3 +1,5 @@
+---
+title: Name Servers | Domains | DNSimple API v2
 excerpt: This page documents the DNSimple domain name servers API v2.
 ---
 