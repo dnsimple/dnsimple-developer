a5b6e7f27ab89fcfbb7adf96339420d7ae39c7eb
Carlos Ribeiro <mail@cadu.dev>
2020-05-18 14:10:46 -0300

    Remove net/http monkey-patch

--- lib/default.rb
+++ lib/default.rb
@@ -4,20 +4,6 @@ require 'json'
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::XMLSitemap
 
-# TODO: Remove this monkey-patch if https://github.com/ruby/ruby/pull/3117 get merged.
-module Net
-  class HTTPResponse
-    class << self
-      def read_status_line(sock)
-        str = sock.readline
-        m = /\AHTTP(?:\/(\d+\.?\d*))?\s+(\d\d\d)(?:\s+(.*))?\z/in.match(str) or
-          raise Net::HTTPBadResponse, "wrong status line: #{str.dump}"
-        m.captures
-      end
-    end
-  end
-end
-
 ROOT = File.expand_path("../../", __FILE__)
 
 def read_http_fixture(filename)
