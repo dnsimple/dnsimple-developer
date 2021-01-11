e18011c9aac20edb6036f9ce27306d361895606f
Sebastian Hermida <sbastn@users.noreply.github.com>
2021-01-11 10:28:45 +0100

    Update and cleanup styles (#309)

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   },
   plugins: [
     new CopyPlugin([
+      { from: 'node_modules/tachyons/css/tachyons.min.css', to: 'tachyons/css/tachyons.min.css' },
       { from: 'node_modules/@fortawesome/fontawesome-free/css/all.min.css', to: 'fontawesome/css/all.min.css' },
       { from: 'node_modules/@fortawesome/fontawesome-free/webfonts', to: 'fontawesome/webfonts', ignore: ['*.svg'] },
     ]),
