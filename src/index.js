88ee528b8b93e2737fc591ce5fbe4fec8c0ee7a1
Sebastian Hermida <sebas00@gmail.com>
2021-04-05 17:47:17 +0200

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import Vue from "vue/dist/vue.esm.js"
 import Tabs from "./components/tabs.vue"
 
-// Troubleshoot with this
-Vue.config.debug = true
-Vue.config.devtools = true
-
 var app = new Vue({
   el: '.component',
   components: {
