24b0311da7253273ddc7dc0e360bcac0f15e9975
Sebastian Hermida <sbastn@users.noreply.github.com>
2022-07-15 10:04:53 +0200

    Adds search (#413)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue/dist/vue.esm.js"
 import Tabs from "./components/tabs.vue"
+import { Search } from "./search.js"
 
 var app = new Vue({
   el: '.component',
@@ -7,3 +8,10 @@ var app = new Vue({
     Tabs,
   },
 })
+
+// Search
+const container = document.getElementById('main');
+const input = document.getElementById('input-search');
+const search = Search(container, input);
+
+search.init();
