d50ab5324b414d4a008acd057731fe199de7728a
Sebastian Hermida <sbastn@users.noreply.github.com>
2022-07-18 09:18:05 +0200

    Dark theme (#414)

--- src/index.js
+++ src/index.js
@@ -15,3 +15,23 @@ const input = document.getElementById('input-search');
 const search = Search(container, input);
 
 search.init();
+
+function toggleColorMode (e) {
+  // Switch to Light Mode
+  if (e.currentTarget.classList.contains("light--hidden")) {
+    document.documentElement.setAttribute("color-mode", "light");
+    localStorage.setItem("color-mode", "light")
+    return;
+  }
+
+  // Switch to Dark Mode
+  document.documentElement.setAttribute("color-mode", "dark");
+  localStorage.setItem("color-mode", "dark");
+};
+
+const toggleColorButtons = document.querySelectorAll(".color-mode__btn");
+console.log(toggleColorButtons);
+
+toggleColorButtons.forEach(btn => {
+  btn.addEventListener("click", toggleColorMode);
+});
