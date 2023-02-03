9df2b14d890e50c2dc4a284e05dee03dc1b6e20d
Sebastian Hermida <sebas00@gmail.com>
2023-02-03 13:14:44 +0100

    Updates webpack plugin copy syntax

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,14 @@ module.exports = {
     ],
   },
   plugins: [
-    new CopyPlugin([
-      { from: 'node_modules/highlightjs/styles/github.css', to: 'highlightjs/styles/github.css' },
-      { from: 'node_modules/tachyons/css/tachyons.min.css', to: 'tachyons/css/tachyons.min.css' },
-      { from: 'node_modules/@fortawesome/fontawesome-free/css/all.min.css', to: 'fontawesome/css/all.min.css' },
-      { from: 'node_modules/@fortawesome/fontawesome-free/webfonts', to: 'fontawesome/webfonts', ignore: ['*.svg'] },
-    ]),
+    new CopyPlugin({
+      patterns: [
+        { from: 'node_modules/highlightjs/styles/github.css', to: 'highlightjs/styles/github.css' },
+        { from: 'node_modules/tachyons/css/tachyons.min.css', to: 'tachyons/css/tachyons.min.css' },
+        { from: 'node_modules/@fortawesome/fontawesome-free/css/all.min.css', to: 'fontawesome/css/all.min.css' },
+        { from: 'node_modules/@fortawesome/fontawesome-free/webfonts', to: 'fontawesome/webfonts', globOptions: { ignore: ['*.svg'] } },
+      ]
+    }),
     new VueLoaderPlugin(),
   ],
 };
