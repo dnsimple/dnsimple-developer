c3bb75e50fc86aafabe504d91ba15d7304848028
David Morrow <dperrymorrow@users.noreply.github.com>
2024-02-21 11:47:31 -0800

    Dynamically loading the "whats new" (#553)
    * dynamically loading the "whats new"
    
    * ensuring that there is an illustration for the main feature
    
    * do not infer hash key
    
    whatever ruby is doing the deploy does not like that
    
    * img tags do not need closed
    
    * reverting formatting changes

--- lib/default.rb
+++ lib/default.rb
@@ -21,3 +21,18 @@ def pretty_print_fixture(filename)
   body = response.read_body
   body ? JSON.pretty_generate(JSON.parse(body)) : nil
 end
+
+def blog_articles
+  uri = URI('https://blog.dnsimple.com/feed.json')
+  res = Net::HTTP.get(uri)
+  feed = JSON.parse(res, { symbolize_names: true })
+  features = feed[:items].select { |item| item[:tags].downcase.include? "feature" }
+
+  main = features.find { |item| item[:illustration] }
+  secondary = features.select { |item| item[:title] != main[:title] }
+
+  {
+    main: main,
+    secondary: secondary.take(2)
+  }
+end
