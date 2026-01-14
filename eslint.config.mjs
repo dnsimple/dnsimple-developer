e5d54113452686b8b7f83e5c006c9628fd533b35
Sebastian Hermida <sebas00@gmail.com>
2026-01-14 10:00:39 +0100

    Apply the same rules that we use in dnsimple-app

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,14 +17,20 @@ export default [
       },
     },
     rules: {
-      'no-unused-vars': ['error', { argsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' }],
-      'vue/multi-word-component-names': 'off',
-      'vue/max-attributes-per-line': 'off',
-      'vue/html-self-closing': 'off',
-      'vue/singleline-html-element-content-newline': 'off',
-      'vue/v-bind-style': 'off',
-      'vue/attributes-order': 'off',
-      'vue/no-v-html': 'off',
+      semi: ['error', 'always'], // Enforce semicolons
+      curly: ['error', 'multi'], // Enforce curly braces only for multi-line blocks
+      'jest/valid-title': 'off', // To be removed
+      'jest/no-standalone-expect': 'off', // To be removed
+      'jest/no-disabled-tests': 'off', // To be removed
+      'jest/no-conditional-expect': 'off', // To be removed
+      'jest/valid-expect': 'off', // To be removed
+      'jest/no-test-prefixes': 'off', // To be removed
+      'jest/no-export': 'off', // To be removed
+      'jest/no-alias-methods': 'off', // To be removed
+      'jest/expect-expect': 'off', // To be removed
+      'jest/valid-describe-callback': 'off', // To be removed
+      'vue/require-v-for-key': 'off', // To be removed
+      'vue/no-reserved-component-names': 'off', // To be removed
     },
   },
   {
